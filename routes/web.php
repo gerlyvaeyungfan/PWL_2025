68f6be552fd434d13bbf58aaca139882733b7d21
gerly <gherly.ylrehg@gmail.com>
2025-03-13 13:33:33 +0700

    Jobsheet 2 - Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
+
+Route::get('/', [HomeController::class]);
+Route::get('/about', [AboutController::class]);
+Route::get('/article', [ArticleController::class]);
+Route::resource('photos', PhotoController::class);
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +23,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return ('Pos ke- '.$postId." Komentar ke-: ".$commentId);
 });
+
+Route::get('/article/{article}', function ($articleId) {
+    return ('Halaman Artikel dengan ID : '.$articleId);
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return ('Nama saya '.$name);
+});
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+Route::get('/about', function () {
+    return 'NIM: 2341760195. [Nama: Gerly Vaeyungfan]';
+});
+
+Route::get('/article', function () {
+    return ('Halaman Artikel');
+});
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+    Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
